e8b82b2b7d4f982b0230885f8ddc8f33c1b1de8c
Halil İbrahim Direktör <hidirektor@gmail.com>
2022-11-05 21:23:06 +0300

    Update
    Port control.

--- src/main/java/me/t3sl4/tfencryption/Runner.java
+++ src/main/java/me/t3sl4/tfencryption/Runner.java
@@ -2,9 +2,18 @@ package me.t3sl4.tfencryption;
 
 import me.t3sl4.tfencryption.Main.TFEncoder;
 
+import java.net.ServerSocket;
+
 public class Runner {
 
     public static void main(String[] args) {
         TFEncoder.main(args);
+    }
+
+    public static boolean checkServerSocket(ServerSocket ss) {
+        if(ss == null) {
+            return false;
+        }
+        return true;
     }
 }
